601b96e6adb2848420617903e7ddc7d87bbad8b5
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-05-12 17:47:36 +0200

    Création de la fonction sendData

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ emailValidation(email);
  */
 const textareaValidation = (textarea) => {
   textarea.addEventListener("input", (e) => {
+    // Suppression du comportement par défaut
     e.preventDefault();
     if (textarea.value == "") {
       textarea.style.border = "2px solid #ff0000";
@@ -98,3 +99,37 @@ const textareaValidation = (textarea) => {
 
 // Appel de la fonction textareaValidation
 textareaValidation(textarea);
+
+// Déclaration de la fonction sendData permettant d'envoyer les données
+const sendData = () => {
+  // Ecoute de l'événement "click" sur le bouton "Envoyer !"
+  submitBtn.addEventListener("click", (e) => {
+    // Suppression du comportement par défaut
+    e.preventDefault();
+    if (
+      !lastName.value.match(regexName) ||
+      !email.value.match(regexEmail) ||
+      textarea.value == ""
+    ) {
+      alert(
+        "Veuillez remplir correctement tous les champs du formulaire correctement !"
+      );
+    } else {
+      const contact = {
+        lastName: lastName.value,
+        email: email.value,
+        textarea: textarea.value,
+      };
+      console.log(contact);
+      alert("Votre message a bien été envoyé !");
+      // Rechargement de la page
+      window.location.reload();
+      lastName.value = "";
+      email.value = "";
+      textarea.value = "";
+      window.scrollTo(0, 0);
+    }
+  });
+};
+// Appel de la fonction sendData()
+sendData();
